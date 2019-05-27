4aae4ba7e90bd02d7a970c91372995b8805d8b80
Lucas Portet <34057690+l-portet@users.noreply.github.com>
2019-05-27 15:32:46 +0200

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ module.exports = {
     headless: true,
     userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
     acceptLanguage: 'fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,la;q=0.6',
-    outputFilename: 'plop',
-    outputFormat: 'csv',
+    outputFilename: 'output',
+    outputFormat: 'csv', // supported format : "json", "csv"
     maxResults: -1,
     puppeteerArgs: [],
     baseURL: 'https://www.pagesjaunes.fr',
